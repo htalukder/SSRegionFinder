f3885114418a6863cc07b943f33abb18a99510f8
htalukder <htalukder@math.umd.edu>
2014-06-10 13:35:52 -0400

    Update SplinesCode.r

--- SplinesCode.r
+++ SplinesCode.r
@@ -1,156 +1,118 @@
-require(gss)
-require(pracma)
+
+library(gss)
+library(pracma)
+
+load(SampleData.rDa)
+
+#Make Data, Run original data and produce fit and standard errors
+
+SSfit <- function(data, response, group, series, id)
+	{
+		dat=data.frame(response=data[,response], group=factor(data[,group]), series=data[,series], id=data[,id])
+		mod=ssanova(response ~ series * group, data=dat)
+		full_x = seq(min(dat$series), max(dat$series), by=1)
+		fit = predict(mod, data.frame(series=full_x, group=factor(1)), inc=c("group", "series:group"), se=TRUE)
+		return(list(data=dat, fit=fit$fit, se=fit$se, x_seq=full_x))
+	}
 
 
-##permTestNew function computes the 1000 different permutations for class
-
-permTestNew<-function(dat,t=0,B=1000){
-  counts = dat$resp;
-  time   = dat$posi;
-  statusP= dat$cl;
-  mat = array(dat$cl,dim=c(length(data$cl),B))
-  tmp = strsplit(rownames(dat),split="")
-  #tmp2 = sapply(tmp,function(i){
-    #x = i[[1]][1]
-      #for(j in 2:4){ x=paste(x,i[j],sep="")}
-    #return(x)})
-  tmp2=dat$inde
-  
-  k = match(unique(tmp2),tmp2)
-  cl  = statusP[k+3];
-  
-  for(l in 1:B){
-    clp = sample(cl,length(cl));
-
-    for(j in 1:(length(k)-1)){
-      statusP[k[j]:k[j+1]]=clp[j]
-    }
-    statusP[k[j+1]:length(statusP)]=clp[j+1]
-    
-    mat[,l] = statusP;
-  }
-  dat$statusp = mat;
-  
-  return(dat)
-}
-
-
-####SSRegionFinder uses SSANOVA to find regions of difference. 
-###Function takes in data with response, class, Individual ID, position, and perm # for to perform
-
-
-library(graphics)
-library(tableplot)
-
-SSRegionFinder=function(data, Y, cl, position, permMat, Perm){
-
-require(gss)
-require(pracma)
-
-#resp=data[,Y]
-#cla=factor(data[,cl])
-#inde=data[,indID]
-#posi=data[,position]
-dat=data.frame(resp=data[,Y], cla=factor(data[,cl]), posi=data[,position])
-mod=ssanova(resp~posi*cla,data=dat)
-x_seq=seq(min(dat$posi),max(dat$posi),by=1)
-predP=predict(mod,data.frame(posi=x_seq, cla=factor(1)), se=T,  inc=c("cla","posi:cla"))
-
-Region_pos=which((2*predP$fit+(1.645*2*predP$se))<0)
-
-if(length(Region_pos)>0){
-
-if(sum(diff(Region_pos)>1)<1){
-	k_pos=list()
-	k_pos[[1]]=Region_pos
-}else{
-	k_pos=list()
-	useInd=which(diff(Region_pos)>1)
-	i=1
-	while (i<=length(useInd))
-		{
-			k_pos[[i]]=Region_pos[1:useInd[i]]
-			Region_pos=Region_pos[-(1:useInd[i])]	
-			i=i+1	
+#Permutation matrix (Input: Ready made data)
+SSmakePerm=function(Data_Ready)
+	{
+		dat2=data.frame(group=factor(Data_Ready$group), id=Data_Ready$id)
+		count=table(dat2$id)
+		samp=unique(dat2)[,1]
+		permMat=list()
+		
+		for (j in 1:1000){
+			new_samp=sample(samp, replace=FALSE)
+			permMat[[j]]=unlist(sapply(1:length(new_samp), function(i){rep(new_samp[i], count[i])}))
 		}
-	i=length(k_pos)+1
-	k_pos[[i]]=Region_pos
-	}
-}else{
-	k_pos=c()
+		return(permMat)	
 	}
 
-Region_neg=which((2*predP$fit-(1.645*2*predP$se))>0)
-if(length(Region_neg)>0){
+#Analysis of all permutations
+SSPermAnalysis=function(Data_Ready, PermList, ResultMatrix, x_seq){
+	result_perm=matrix(0, 1000, nrow(ResultMatrix))
+	x_perm=x_seq
+	dat_perm=Data_Ready
 
-if(sum(diff(Region_neg)>1)<1){
-	k_neg=list()
-	k_neg[[1]]=Region_neg
-}else{
-	k_neg=list()
-	useInd=which(diff(Region_neg)>1)
-	i=1
-	while (i<=length(useInd))
-		{
-			k_neg[[i]]=Region_neg[1:useInd[i]]
-			Region_neg=Region_neg[-(1:useInd[i])]	
-			i=i+1	
+		for (j in 1:1000){
+			dat_perm$group=PermList[[j]]
+			mod_perm=ssanova(response ~ series * group, data=dat_perm)
+			fit_perm = cbind(x_perm, abs(2*predict(mod_perm, data.frame(series=x_perm, group=factor(1)), inc=c("group", "series:group"), se=TRUE)$fit))
+				for (i in 1:nrow(ResultMatrix)){
+					area_perm=fit_perm[which(fit_perm[,1]==ResultMatrix[i,1]) : which(fit_perm[,1]==ResultMatrix[i, 2]), ]
+					result_perm[j, i]=trapz(x=area_perm[,1], y=area_perm[,2])
+				}
+			if (j%%100==0){
+				show(j)
+			}	
 		}
-	i=length(k_neg)+1
-	k_neg[[i]]=Region_neg
+	return(result_perm)
+}
+
+
+#Check if any region where curve is above 0
+#input (fit, se, sequence, above or below line 0)
+SSRegions_candidate=function(fit, stand.error, x_seq, above_zero=TRUE){
+	if (above_zero){
+	diff_position = which((2*fit-(1.96*2*stand.error))>0)
+	}else{
+		diff_position = which((2*fit+(1.96*2*stand.error))<0)
 	}
-}else{
-	k_neg=c()
-	}
-
-k=c(k_pos,k_neg)
-
-#return(k)
-#}
-
-#if (k>0){
-
-if (length(k)>0){
-
-Result=matrix(0, nrow=length(k), ncol=4)
-colnames(Result)=c("Start", "End", "Area", "P-Value")
-
-for (i in 1:nrow(Result)){
-Result[i,1]=min(k[[i]])
-Result[i,2]=max(k[[i]])
-Result[i,3]=trapz(x=x_seq[min(k[[i]]):max(k[[i]])], y=abs(2*predP$fit[min(k[[i]]):max(k[[i]])]))
+	if (length(diff_position)>0){
+		reg_index=which(diff(diff_position)!=1)
+		reg_position=matrix(0, (length(reg_index)+1), 4)
+			if (length(reg_index)==0){
+				reg_position[1,1]=x_seq[diff_position[1]]
+				reg_position[1,2]=x_seq[tail(diff_position, n=1)]
+			}else{
+				i=1
+				while(length(reg_position)!=0 & length(reg_index)!=0){
+					reg_position[i, 1]=x_seq[diff_position[1]]
+					reg_position[i,2]=x_seq[diff_position[reg_index[1]]]
+					diff_position=diff_position[-c(1:reg_index[1])]
+					reg_index=reg_index[-1]
+					i=i+1
+				}
+			reg_position[i, 1]=x_seq[diff_position[1]]
+			reg_position[i, 2]=x_seq[tail(diff_position, n=1)]
+			}
+		}else{
+			reg_position=NULL	
+	}	
+	return(reg_position)	
 }
 
-dat2=dat
-permMat=permMat
-#fine up to here
-#return(Result)
-#}
 
-areaPSp=matrix(0, 1000, nrow(Result))
-
-for (i in 1:Perm){
-
-        dat2$cla=factor(permMat[,i])
-        mod=ssanova(resp~posi*cla, data=dat2)
-        predPl=predict(mod, data.frame(posi=x_seq, cla=factor(1)), se=T, inc=c("cla","posi:cla"))
-        	for (j in 1:nrow(Result)){
-					areaPSp[i, j]=trapz(x=x_seq[Result[j,1]:Result[j,2]],  y=abs(2*predPl$fit[Result[j,1]:Result[j,2]]))
-						}				        
-        #show(i)
+SSRegion_Finder=function(data, response, group, series, id ){
+	set.seed(123)
+	dat_original=SSfit(data=data, response=response, group=group, series=series, id=id)
+	#perm_mat=SSmakePerm(dat_original$data)
+	index_pos=SSRegions_candidate(fit=dat_original$fit, stand.error=dat_original$se, x_seq=dat_original$x_seq, above_zero=TRUE)
+	index_neg=SSRegions_candidate(fit=dat_original$fit, stand.error=dat_original$se, x_seq=dat_original$x_seq, above_zero=FALSE)
+	index_all=rbind(index_pos, index_neg)
+	
+	if (nrow(index_all)>0){
+		colnames(index_all)=c("Region Start", "Region End", "Area", "P-Value")
+		predict_area=cbind(dat_original$x_seq , abs(2*dat_original$fit))
+		perm_list=SSmakePerm(dat_original$data)
+		PermResult=SSPermAnalysis(Data_Ready= dat_original$data, PermList=perm_list, ResultMatrix=index_all, x_seq=dat_original$x_seq )
+		for (i in 1:nrow(index_all)){
+			area_orig=predict_area[which(predict_area[,1]==index_all[i,1]):which(predict_area[,1]==index_all[i, 2]), ]
+			actual_area=trapz(x=area_orig[,1], y=area_orig[,2])
+			index_all[i, 3]=actual_area
+			index_all[i, 4]=1-(length(which(actual_area>PermResult[,i]))/1000)
+		}
+	
+	}else{
+		return("No Regions Found")}
+	
+	return(list(Result=index_all, data.frame=dat_original$data, fit=2*dat_original$fit, se=2*dat_original$se))
 }
 
-for (i in 1:nrow(Result)){
-	Result[i,4]=mean(Result[i,3]<areaPSp[,i])
-	Result[i,1]=Result[i,1]-14
-    Result[i,2]=Result[i,2]-14
-}
 
-#Here its fine
-#if (length(k)==0){
-#return("No Region Found")}else{
-	return (list(Result, areaPSp))}else{
-	return("No significant region found")
-}
-}
+# Test function RUN THIS#####
 
+res=SSRegion_Finder(data=data, response=3, group=2, series=1, id=4)
